896a7bb13604de24b1e0a54996e47644aa2b27c1
said <said@192.168.1.17>
2022-11-13 18:35:59 +0000

    kata 1

---
+++ src/main/java/com/mycompany/kata1/Person.java
@@ -0,0 +1,32 @@
+
+package com.mycompany.kata1;
+
+/**
+ *
+ * @author said
+ */
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+    
+    public int getAge() {
+        return (int) Period.between(birthdate, LocalDate.now()).getYears();
+    }
+}

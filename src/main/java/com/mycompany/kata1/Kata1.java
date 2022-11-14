45de976e5911c1837309e3ef0d791b37a005d2d7
said <said@192.168.1.17>
2022-11-14 17:13:32 +0000

    kata 1

--- src/main/java/com/mycompany/kata1/Kata1.java
+++ src/main/java/com/mycompany/kata1/Kata1.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class Kata1 {
 
     public static void main(String[] args) {     
-        Person person = new Person("Saidd", LocalDate.of(2001, 4, 3));
+        Person person = new Person("Said", LocalDate.of(2001, 4, 3));
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
 }
